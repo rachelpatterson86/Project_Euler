aa23144a78a31926549bde931b61b35223cc265c
Rachel Patterson <rachelpatterson@Thornes-MacBook-Air.local>
2016-02-16 21:38:26 -0500

    updated comments

--- 001_Multiples_of_3_and_5.rb
+++ 001_Multiples_of_3_and_5.rb
@@ -1,15 +1,12 @@
+#Multiples of 3 and 5
+
+#Problem 1 - https://projecteuler.net/problem=1
+
 # If we list all the natural numbers below 10 that are multiples of 3 or 5,
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-# Find the sum of all the multiples of 3 or 5 below 1000.
-def test
-  puts "sum of multiples below 10 is 23", multiples_3_5(10)
-  puts "sum of multiples below 11 is 33", multiples_3_5(11)
-  puts "sum of multiples below 12 is 33", multiples_3_5(12)
-  puts "sum of multiples below 13 is 45", multiples_3_5(13)
-end
-
 def multiples_3_5(num)
+  original = num
   num -= 1
   sum_3_5 = 0
   num.times do
@@ -18,7 +15,12 @@ def multiples_3_5(num)
     end
       num -= 1
   end
-  puts "The sum of numbers below #{num} is #{sum_3_5}"
+  puts "The sum of numbers below #{original} and are multiples of 3 and 5 is #{sum_3_5}"
 end
 
 multiples_3_5(1000)
+
+#To find the sum of all multiples of 3 OR 5 below a given number, the function must check
+# if a) the number testing is evenly divisible by 3 or 5. b) if so, then that value
+#should be incremented into a sum variable that will hold the sums of the numbers that
+#meet condition a.
